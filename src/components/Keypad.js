3c7bbc9104386a7e85e64b1c04dadd87c596354a
loretta-dhahabu <loreydjefwa@gmail.com>
2022-07-14 18:32:30 +0300

    commit tree 62b100180d507aa7b8d0c86f3e90359788c95726

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,23 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad ()
+{
+    function keypadFn ( eventFunction )
+    {
+        console.log(eventFunction.target.name);
+        // console.log("Entering password...");
+    }
+    return (
+      <>
+        <input
+          type="password"
+          name="Entering password..."
+          onChange={keypadFn}
+          placeholder="Enter your password..."
+        />
+      </>
+    );
+    
+}
+export default Keypad;
